0dc096c66aef6f4b6ca333290416c37062db056a
Juliya Boiko <boikojuliya2707@gmail.com>
2022-09-19 15:43:05 +0400

    add function listContacts

---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const operationsPath = path.join(__dirname, 'contacts.js');
+const contactsOperations = require(operationsPath);
+
+// const { Command } = require("commander");
+// const program = new Command();
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
+
+// program.parse(process.argv);
+
+// const argv = program.opts();
+
+// // TODO: рефакторить
+ async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await contactsOperations.listContacts();
+      console.log(list);
+      break;
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      await contactsOperations.addContact(name, email, phone);
+      break;
+    case "remove":
+      await contactsOperations.removeContact(id);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type! DEFAULT ACTION");
+  }
+}
+
+//invokeAction({ action: "list" });
+invokeAction({ action: 'get', id: 1 });
+
+
+
+
+// invokeAction(argv);
