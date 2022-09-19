b2a44e687cb408aaa79c6442fc6cee7a68ce24a2
Juliya Boiko <boikojuliya2707@gmail.com>
2022-09-19 15:48:44 +0400

    add function getContactById

---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const filePath = path.join(__dirname, 'db/contacts.json');
+
+async function listContacts() {
+  const data = await fs.readFile(filePath);
+  const contactsList = JSON.parse(data);
+  return contactsList;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => +item.id === contactId);
+  return result;
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+
+
+
+
+
+
+
+
+
+
+//const all = fs.readFile('db/contacts.json').then(data => console.log(JSON.parse(data))).catch(error => console.log(error.message));
+
+// const contactsOperations = async(filePath, action = 'read', value = '') => {
+//   switch(action) {
+//     case 'read' : 
+//       const data = await fs.readFile(filePath);
+//       console.log(JSON.parse(data));
+//       break;
+//     case 'add':
+//       await fs.appendFile(filePath, value);
+//       break;
+//     default:
+//       console.log('Default CASE!!!!!!');
+//   }
+// };
+
+// contactsOperations('db/contacts.json');
